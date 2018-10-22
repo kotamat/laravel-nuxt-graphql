6da8b0636a89e5c81092382028c174c64b621e92
kotamat <kota1861@gmail.com>
2018-10-22 20:50:41 +0900

    [WIP] install gridsome
    gridsome doesn't support laravel graphql endpoint

---
+++ src/pages/users.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout>
+    <h1>Users list</h1>
+    <dl v-for="user in $page.users.data">
+      <dh>{{user.id}}</dh>
+      <dd>{{user.name}}</dd>
+    </dl>
+  </Layout>
+</template>
+
+<page-query>
+query Users {
+  users(count: 10){
+    data {
+      name
+      id
+    }
+  }
+}
+</page-query>
+
